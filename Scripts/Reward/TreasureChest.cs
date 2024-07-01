db06b4a94a426dc2bf3616629df124dbdf09fdd9
Adrian Reid <devilred101@gmail.com>
2024-07-01 13:01:24 +0100

    End of Lesson 8.3 Creating a Reward Resource

---
+++ Scripts/Reward/TreasureChest.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class TreasureChest : StaticBody3D
+{
+    [Export] private Area3D areaNode;       // Area3D node for the player detection area.
+    [Export] private Sprite3D spritenode;   // Sprite3D node for the interaction icon.
+    [Export]private RewardResource reward;  // Use our custom RewardResource.
+
+    public override void _Ready()
+    {
+        // Subscribe to the event and make the icon visible/invisible.
+        areaNode.BodyEntered += HandleBodyEntered;
+        areaNode.BodyExited += HandleBodyExited;
+
+        // Disable engine processes.
+        SetProcessInput(false);
+        SetPhysicsProcess(false);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        // If the input is not INPUT_INTERACT then return.
+        if (!Input.IsActionJustPressed(GameConstants.INPUT_INTERACT)) { return; }
+
+        GD.Print("Chest interacted");
+    }
+
+    private void HandleBodyEntered(Node3D body)
+    {
+        // Make sprite visible and process input
+        spritenode.Visible = true;
+        SetProcessInput(true);
+    }
+
+    private void HandleBodyExited(Node3D body)
+    {
+        // Make sprite invisible and disable input
+        spritenode.Visible = false;
+        SetProcessInput(false);
+    }
+}
